5bb12e53bfea160b7c0bfa1aa42f9c7b0ae97f61
Rohit Palkar <rohit.palkar@dista.ai>
2022-06-07 16:58:17 +0530

    FirstCommit

---
+++ src/app/requestor/requestor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-requestor',
+    templateUrl:'./requestor.component.html',
+})
+
+export class RequestorComponent{
+
+}
